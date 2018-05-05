cfd90b62dae6403761e3b2161c2f78a6b039b094
amoham37 <amoham811@gmail.com>
2018-05-05 16:23:16 -0500

    complete

--- dpwebsite/settings.py
+++ dpwebsite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+                  
+    'dpwebsite.core',
+
 ]
 
 MIDDLEWARE = [
@@ -118,5 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'home'
