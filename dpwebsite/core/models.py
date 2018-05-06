81b09b98871ff927d04ff3159447f7d2746effc9
Naoufal <naoufal.bahloul@yahoo.com>
2018-05-06 17:09:09 -0500

    UPDATED DATABASE SCHEMA

--- dpwebsite/core/models.py
+++ dpwebsite/core/models.py
@@ -6,41 +6,40 @@ from django.db import models
 # Create your models here.
 
 class Users(models.Model):
-    userID = models.CharField(max_length=50)
-    userName = models.CharField(max_length=50)
-    userPassword = models.CharField(max_length=50)
-    userEmail = models.CharField(max_length=50)
-    userAddress = models.CharField(max_length=100)
-    userCity = models.CharField(max_length=30)
-    userZip = models.IntegerField()
-    userState = models.CharField(max_length=2)
-    userFirstName = models.CharField(max_length=50)
-    userLastName = models.CharField(max_length=50)
-    userDegree = models.CharField(max_length=25)
-    userDesiredClasses = models.IntegerField()
+    USER_ID = models.CharField(max_length=50)
+    USER_Name = models.CharField(max_length=50)
+    USER_Password = models.CharField(max_length=50)
+    USER_Email = models.CharField(max_length=50)
+    USER_Address = models.CharField(max_length=100)
+    USER_City = models.CharField(max_length=30)
+    USER_Zip = models.IntegerField()
+    USER_State = models.CharField(max_length=2)
+    USER_FirstName = models.CharField(max_length=50)
+    USER_LastName = models.CharField(max_length=50)
+    USER_Degree = models.CharField(max_length=25)
 
 
 class Courses(models.Model):
-    courseID = models.CharField(max_length=15)
-    courseTitle = models.CharField(max_length=25)
-    courseDescription = models.CharField(max_length=250)
-    courseSubject = models.CharField(max_length=50)
-    courseLevel = models.IntegerField()
+    CRSE_ID = models.CharField(max_length=15)
+    CRSE_TITLE = models.CharField(max_length=25)
+    DESCRIPTION = models.CharField(max_length=250)
+    CRSE_SUBJECT = models.CharField(max_length=50)
+    PREREQUISITE = models.CharField(max_length=250)
 
 
 class PreRequisites(models.Model):
-    preReqID = models.ForeignKey(Courses, related_name='prereq_id', on_delete=models.CASCADE)
-    courseID = models.ForeignKey(Courses, related_name='prereq_course_id', on_delete=models.CASCADE)
+    PREREQUISITE_ID = models.ForeignKey(Courses, related_name='prereq_id', on_delete=models.CASCADE)
+    CRSE_ID = models.ForeignKey(Courses, related_name='prereq_course_id', on_delete=models.CASCADE)
 
 
 class Path(models.Model):
-    userID = models.ForeignKey(Users, related_name='path_userID', on_delete=models.CASCADE)
+    USER_ID = models.ForeignKey(Users, related_name='path_userID', on_delete=models.CASCADE)
     courseID = models.ForeignKey(Courses, related_name='path_courseID', on_delete=models.CASCADE)
     pathQuarter = models.IntegerField()
     pathSteps = models.IntegerField()
 
 
 class Availability(models.Model):
-    courseID = models.ForeignKey(Courses, related_name='avail_courseID', on_delete=models.CASCADE)
+    CRSE_ID = models.ForeignKey(Courses, related_name='avail_courseID', on_delete=models.CASCADE)
     availQuarter = models.IntegerField()
     availDelivery = models.IntegerField()
