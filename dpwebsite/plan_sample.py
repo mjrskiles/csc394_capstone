57dc66eabe0a9edce2cd1da5630e5f3ef02f87c1
Michael Skiles <mjrskiles@gmail.com>
2018-05-20 16:07:35 -0500

    Added plan_sample data

---
+++ dpwebsite/plan_sample.py
@@ -0,0 +1,23 @@
+plan = {
+    'plan': {
+        'courses_complete': ['CSC 400', 'CSC 401', 'CSC 402'],
+        'terms': [
+            {
+                'name': 'Fall Quarter 2018',
+                'courses': ['CSC 403', 'CSC 406']
+            }, 
+            {
+                'name': 'Winter Quarter 2019',
+                'courses': ['CSC 421', 'CSC 430']
+            }, 
+            {
+                'name': 'Spring Quarter 2019',
+                'courses': ['CSC 435', 'CSC 436']
+            },
+            {
+                'name': 'Fall Quarter 2018',
+                'courses': ['CSC 450', 'CSC 475']
+            }
+        ]
+    }
+}
